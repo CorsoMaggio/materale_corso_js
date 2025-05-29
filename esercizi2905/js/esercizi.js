e156bf14980e979df94d666ad09070c4e4e556c1
marikadiblasio <marbla.albram@gmail.com>
2025-05-29 14:30:34 +0200

    esercizio 2905

---
+++ esercizi2905/js/esercizi.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * 1. Stampa in console tutti i numeri da 1 a 10
+ * 
+ * 2. Stampa in console tutti i numeri pari da 1 a 20
+ * 
+ * 3. Riprendi l'elenco della tua classe. Accettando un nome dato dall'utente in un prompt, controlla se quel nome è presente nell'elenco.
+ *    Se è presente stampa in console il messaggio "Puoi accedere", altrimenti "Non Puoi accedere"
+ */
